567a9ce24ef44b19dd9213cdd67d769bce94887b
David Taylor <david@taylorhq.com>
2024-11-29 15:40:34 +0000

    DEV: Convert to native class syntax (#18)

--- javascripts/discourse/components/simple-banner.js
+++ javascripts/discourse/components/simple-banner.js
@@ -3,15 +3,11 @@ import { service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
 import discourseComputed from "discourse-common/utils/decorators";
 
-export default Component.extend({
-  router: service(),
-
-  init() {
-    this._super(...arguments);
-  },
+export default class SimpleBanner extends Component {
+  @service router;
 
   @discourseComputed("router.currentRouteName")
   isHomepage(currentRouteName) {
     return currentRouteName === `discovery.${defaultHomepage()}`;
-  },
-});
+  }
+}
